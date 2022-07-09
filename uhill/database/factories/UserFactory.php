f589f779bbc567993809f2c0fe9bd58b573e48e3
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-07-09 14:40:31 -0700

    Database factory created
    use command php artisan db:seed

--- uhill/database/factories/UserFactory.php
+++ uhill/database/factories/UserFactory.php
@@ -19,10 +19,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
+            'username' =>fake()->unique()->userName,
+            'email' => preg_replace('/@example\..*/', '@learn.vsb.bc.ca', fake()->unique()->safeEmail),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt("12345678"),
             'remember_token' => Str::random(10),
+            'admin' => false
         ];
     }
 
