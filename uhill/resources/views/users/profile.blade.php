abe6532a3c2bb374b1cd1ae7e533ec257689ec88
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-08-06 22:10:57 -0700

    dawwda
    adwdawd

--- uhill/resources/views/users/profile.blade.php
+++ uhill/resources/views/users/profile.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 
+<div class="items-center bg-felixSalmon">
+<div class="p-5 m-5 b-5">
+
+    <div>
+        <h1>username: </h1> <h1>{{$username}}</h1>
+    </div>
+
+    <div>
+        <h2>name: </h2> <h2>{{$name}}</h2>
+    </div>
+
+    <div>
+        <p>date joined: </p> <p>{{$date_joined}}</p>
+    </div>
+
+    <div>
+        <p> description: </p> <p>{{$description}}</p>
+    </div>
+
+    <div>
+        <p>grade: </p> <p>{{$grade}}</p>
+    </div>
+
+    <div><p>Website: </p> <a href="{{$url}}">{{$url}}</a></div>
 
 
-<h1>
-    {{$username}}
-</h1>
 
-<h2>{{$name}}</h2>
-
-<p>{{$date_joined}}</p>
-
-<p>{{$description}}</p>
-
-<p>{{$grade}}</p>
-
-<a href="{{$url}}">{{$url}}</a>
+</div>
+</div>
 
 
 @auth
