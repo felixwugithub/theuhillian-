89fc85e238bbda2ded2705b4a8ec6e59cb88ae91
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-08-06 21:25:48 -0700

    Fixed profile seeding

---
+++ uhill/database/factories/ProfileFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profile>
+ */
+class ProfileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'grade' =>fake()->numberBetween(8,12),
+            'url' => fake()->url,
+            'description' => fake()->paragraph(3),
+        ];
+    }
+}
