a54ab15d318f49bb3ebced1c3affe8baf917ba56
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-08-12 21:48:17 -0700

    The beginning to the game changer
    Will be adding ability for people to public articles

---
+++ uhill/database/migrations/2022_08_13_042726_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('club_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('author');
+            $table->string('title');
+            $table->longText('content');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
