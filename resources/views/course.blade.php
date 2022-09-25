f4b656e1ea75f58d76a8fdad5989a3d62fffa7de
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2022-09-25 10:42:13 -0700

    Emergency fix

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -38,7 +38,7 @@
             <div class="md:w-5/12 md:py-5 md:px-12 m-5">
                 <h1 class="font-sf text-5xl mb-3">{{$course['course_name']}}</h1>
                 <p class="font-sansMid">{{$course['description']}}</p>
-                <a href="../teacher/{{$course->teacher['id']}}">Teacher: {{$course->teacher['name']}}</a>
+                <a class="text-2xl font-comfortaa text-hotPink" href="../teacher/{{$course->teacher['id']}}">Teacher: {{$course->teacher['name']}}</a>
 
                 @auth()
                 <a class="bg-hotPink text-white  w-56 h-14 m-5 text-3xl items-center py-auto font-sf text-center justify-center mx-auto flex rounded-lg" href="/course/{{$course['id']}}/review"> Give review </a>
